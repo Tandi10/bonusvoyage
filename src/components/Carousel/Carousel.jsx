1839008cd48f37d46e1a7eac396aed52b8b504c4
bintou karamoko <bintoukaramoko3@gmail.com>
2017-03-09 15:44:09 +0100

    add compoments

---
+++ src/components/Carousel/Carousel.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Carousel extends React.Component {
+
+    render() {
+        return (
+            <div className='Carousel'></div>
+        );
+    }
+
+}
+
+export default Carousel;
---
+++ src/components/Travel/Travel.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Travel extends React.Component {
+
+    render() {
+        return (
+            <div className="Travel"></div>
+        );
+    }
+
+}
+
+export default Travel;
